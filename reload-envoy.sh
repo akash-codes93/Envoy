26750cb3f393c178947e31a842725799bf44d53d
Akash Gupta <akash5gupta5@gmail.com>
2025-08-04 10:28:10 +0530

    added whitelisted

--- reload-envoy.sh
+++ reload-envoy.sh
@@ -1,12 +1,24 @@
+kubectl -n demo delete configmap jwt-whitelist
+sleep 5
+
+kubectl apply -f jwt-whitelist-configmap.yaml
+sleep 5
+
 kubectl -n demo delete configmap envoy-config
 sleep 5
 
 kubectl apply -f envoy-config-blocked.yaml
 sleep 5
 
-kubectl -n demo rollout restart deployment/envoy-proxy
-
+kubectl -n demo delete deployment envoy-proxy
 sleep 5
+
+kubectl apply -f envoy-deployment-blocklist.yaml
+sleep 5
+
+# kubectl -n demo rollout restart deployment/envoy-proxy
+
+sleep 2
 kubectl -n demo get pods
 
 # add sleep for 5 seconds
